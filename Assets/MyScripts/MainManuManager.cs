63b790c127238c6672f64bde7dd0613eb08c32bf
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2026-01-07 11:43:20 -0300

    Diferentes paneles en el menu principal
    Todo en placeholder

---
+++ Assets/MyScripts/MainManuManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [Header("All Panels")]
+    [SerializeField] private GameObject[] panels;
+
+    private void Start()
+    {
+        ShowPanel(panels[0]); //La pantalla principal, no se si se puede trabar en uno, por las dudas
+    }
+
+    public void ShowPanel(GameObject panelToShow)
+    {
+        foreach (var panel in panels)
+        {
+            panel.SetActive(panel == panelToShow);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Saliendo del juego");
+        Application.Quit();
+    }
+}
