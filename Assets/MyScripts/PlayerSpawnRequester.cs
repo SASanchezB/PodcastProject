09e5867aecfe5499387cf103b10928d606fa4a3f
Santiago Sanchez <santiago.sanchez@davinci.edu.ar>
2026-01-30 11:33:55 -0300

    Revert "Solucion de error (se le saco rng, se agregara si se puede)"
    This reverts commit 3c072631c4e2ef8554896b8c596d9bc620afc37e.

--- Assets/MyScripts/PlayerSpawnRequester.cs
+++ Assets/MyScripts/PlayerSpawnRequester.cs
@@ -1,5 +1,5 @@
-ï»¿using Unity.Netcode;
 using UnityEngine;
+using Unity.Netcode;
 
 public class PlayerSpawnRequester : NetworkBehaviour
 {
@@ -7,9 +7,18 @@ public class PlayerSpawnRequester : NetworkBehaviour
     {
         base.OnNetworkSpawn();
 
-        if (IsServer && PlayerSpawnSystem.Instance != null)
+        // SOLO en el servidor hacemos la asignación (evita race/client-side moves)
+        if (IsServer)
         {
-            PlayerSpawnSystem.Instance.OnPlayerObjectSpawned(NetworkObject);
+            // Pedimos al sistema central que nos coloque en un spawn porque si no, se sobrepone
+            if (PlayerSpawnSystem.Instance != null)
+            {
+                PlayerSpawnSystem.Instance.AssignSpawnForPlayer(this.NetworkObject);
+            }
+            else
+            {
+                Debug.LogWarning("PlayerSpawnRequester: PlayerSpawnSystem.Instance es null.");
+            }
         }
     }
 }
